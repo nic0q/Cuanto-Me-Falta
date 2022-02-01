c88a41a83dacbe2f804742aafae84bf548a4bc3f
Nícolas Cheneaux <nicolas.farfan.ch@usach.cl>
2022-02-01 00:01:24 -0300

    Avances en el diseÃo, estructura

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const $but = document.getElementById("button")
+const $add = document.getElementById("button")
 const $calc = document.getElementById("calculo")
+const $del = document.getElementById("delete")
 const $card = document.getElementById("card")
 const $cards = document.getElementById("cards")
-const a = ()=>{
+const addCard = ()=>{
   const ab = $card.cloneNode(true)
-  var randomColor = Math.floor(Math.random()*16777215).toString(16);
-  ab.style.color = "red";
   $cards.insertAdjacentElement("afterbegin",ab)
 }
-
-$but.onclick = a
+const deleteCard = () =>{
+  try{
+    $cards.children[$cards.children.length-1].remove()
+  }
+  catch{
+    alert("Todo ya fue eliminado")
+  }
+}
+const calculate = () =>{
+  let arr = []
+  for (let i = 0 ; i < $cards.children.length; i++){
+    arr[i] = $cards.children[i].nodeValue
+  }
+  console.log(arr)
+}
+$add.onclick = addCard
+$del.onclick = deleteCard 
+$calc.onclick = calculate
