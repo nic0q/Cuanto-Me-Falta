1c2deb1a6c441a4ca1bbe03651eb57dd59996f09
Nícolas Cheneaux <nicolas.farfan.ch@usach.cl>
2022-02-02 12:45:07 -0300

    Correciones Varias Se corrigen bugs con la funcionalidad "reset", se modifica el readme y se corrigen errores con los manejo de errores de las funcionalidades (Cuando son negativos)

--- README.md
+++ README.md
@@ -2,20 +2,21 @@
 Página para calcular la nota final, de todas las ponderaciones y notas de un ramo, en la escala del 1 y 7
 
 ### Instrucciones 
-- Para añadir una nueva nota|tarjeta, presionar el botón "Nueva Nota".
-- Para eliminar una nota, presionar "Eliminar Nota".
-- Para calcular la ponderación, presionar "Calcular".
+- Para añadir una nueva nota|tarjeta, presionar el botón "Nueva Nota"
+- Para eliminar una nota, presionar "Eliminar Nota"
+- Para volver al estado inicial (eliminar todo) "Reset"
+- Para calcular la ponderación, presionar "Calcular"
 
 #### La página principal se ve de la siguiente forma:
 
-![Captura de pantalla 2022-02-01 175140](https://user-images.githubusercontent.com/91075814/152049384-fa9943d7-4e9c-4b41-a120-86798d138970.png)
+![image](https://user-images.githubusercontent.com/91075814/152186882-416188de-ebb6-4fba-9ff8-a89ba8322a27.png)
 
 #### Cada tarjeta posee dos campos, la primera para ponderación en porcentaje (%) y otra para la nota (Del 1 al 7)
 - Solo debe rellenar estos campos con su nota y ponderación correspondiente
 
 #### Un ejemplo:
 
-![image](https://user-images.githubusercontent.com/91075814/152050155-234887d7-4f31-477c-8e16-0f4e23c0e5d2.png)
+![image](https://user-images.githubusercontent.com/91075814/152187043-ced8eab3-a56c-4aa8-8990-b3b90bc9f2c9.png)
 
 
 **Espero sea de utilidad 😆**
--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+// Buttons
 const $add = document.getElementById("button")
 const $calc = document.getElementById("calculo")
 const $del = document.getElementById("delete")
 const $delAll = document.getElementById("deleteAll")
+// Cards, elements
 const $card = document.getElementById("card")
 const $cards = document.getElementById("cards")
 const $ap = document.getElementById("aprobar")
 const $des = document.getElementById("desaprobar")
 const $error = document.getElementById("error")
-
+// addCard: Funcion que añade una nueva tarjeta
 const addCard = () =>{
-  const ab = $card.cloneNode(true)
-  ab.classList.toggle("d-none")
-  $cards.insertAdjacentElement("beforebegin",ab)
+  const modelCard = $card.cloneNode(true) // Se toma una tarjeta como modelo para clonarla y spawmear las demas
+  modelCard.classList.toggle("d-none")
+  $cards.insertAdjacentElement("beforebegin",modelCard)
 }
+// deleteCard: Funcion que elimina una tarjeta
 const deleteCard = () =>{
   try{
     const len = document.querySelectorAll(".card").length-2
@@ -22,7 +25,28 @@ const deleteCard = () =>{
     console.log("Todo eliminado")
   }
 }
-// Hacer el dnone con bootstrap para que tire un mensaje con toggle
+// DeleteAll: Funcion que resetea todas las notas y reinicia a el estado inicial
+const deleteAll = () =>{
+  if(!$ap.classList.contains("d-none")){
+    $ap.classList.toggle("d-none")
+  }
+  if(!$error.classList.contains("d-none")){
+    $error.classList.toggle("d-none")
+  }
+  if(!$des.classList.contains("d-none")){
+    $des.classList.toggle("d-none")
+  }
+  for(e of document.querySelectorAll(".card")){
+    if(document.querySelectorAll(".card").length === 1){
+      break
+    }
+    else{
+      e.remove()
+    }
+  }
+}
+// Calculate: Funcion que calcula el promedio total de las notas, y maneja posibles errores:
+//            (Si el promedio es negativo, Si el promedio es 0, Si hay tarjetas vacias)
 const calculate = () =>{
   const pond = []
   const nota = []
@@ -44,7 +68,7 @@ const calculate = () =>{
     if(!$des.classList.contains("d-none")){
       $des.classList.toggle("d-none")
     }
-    if(!$ap.classList.contains("d-none")){
+    else if (!$ap.classList.contains("d-none")){
       $ap.classList.toggle("d-none")
     }
     $error.classList.remove("d-none")
@@ -58,7 +82,18 @@ const calculate = () =>{
         $error.classList.toggle("d-none")
       }
       $des.classList.remove("d-none")
-      $des.innerHTML = `Ingrese alguna nota`
+      $des.innerHTML = `Alguna nota es 0`
+    }
+    else if(res < 0){
+      if(!$error.classList.contains("d-none")){
+        $error.classList.toggle("d-none")
+      }
+      else if (!$ap.classList.contains("d-none")){
+        $ap.classList.toggle("d-none")
+      }
+      $des.classList.remove("d-none")
+      $des.innerHTML = `Ingrese solo numeros positivos`
+
     }
     else if(res >= 3.96){
       if(!$des.classList.contains("d-none")){
@@ -78,15 +113,11 @@ const calculate = () =>{
         $error.classList.toggle("d-none")
       }
       $des.classList.remove("d-none")
-      $des.innerHTML = `Su nota es ${res.toFixed(3)}`
+      $des.innerHTML = `Su nota es ${res.toFixed(3)}, para llegar a 4, le falta ${3.96-res.toFixed(3)}`
     }
   }
 }
-const deleteAll = () =>{
-  document.querySelectorAll(".card").forEach(e =>{
-    e.remove()
-  })
-}
+
 $add.onclick = addCard
 $del.onclick = deleteCard 
 $calc.onclick = calculate
